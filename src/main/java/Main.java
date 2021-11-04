8b7d55033b6b2b53445dd3a3b630fa9499a407c1
Silvernika <92356011+Silvernika@users.noreply.github.com>
2021-11-04 09:33:18 +0300

    4 11 2021

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("test");
+        BullsAndCows bullsAndCows = new BullsAndCows();
+
+        Scanner scanner = new Scanner(System.in);
+
+        String yesOrNo;
+
+        boolean validInput = false;
+        boolean game;
+
+        do {
+            bullsAndCows.guess();
+
+            game = false;
+
+            System.out.print("Do you want to play again: yes or no? ");
+
+            yesOrNo = scanner.nextLine();
+
+            if (yesOrNo.equalsIgnoreCase("yes") || yesOrNo.equalsIgnoreCase("no")) {
+                validInput = true;
+            }
+
+            if (yesOrNo.equalsIgnoreCase("yes")) {
+                game = true;
+            }
+
+            if (!validInput) {
+                System.out.println("Invalid input. Try again");
+            }
+        } while (validInput && game);
     }
 }
